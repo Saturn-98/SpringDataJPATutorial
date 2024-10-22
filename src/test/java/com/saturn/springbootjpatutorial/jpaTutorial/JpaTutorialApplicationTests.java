adc2665cd7d96686879c9b05080478b0e7d8151f
Saturn-98 <ishaan98jaiswal@gmail.com>
2024-10-22 10:20:40 +0530

    Initial commit

---
+++ src/test/java/com/saturn/springbootjpatutorial/jpaTutorial/JpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saturn.springbootjpatutorial.jpaTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
