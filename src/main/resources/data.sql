6c9963c8bc41ea7bc4cdb6ad09bcd871da03adb3
Saturn-98 <ishaan98jaiswal@gmail.com>
2024-10-22 11:47:56 +0530

    Chapter 3.2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO test.product_table(price, quantity, sku, title_x)
+VALUES
+(12.4, 4, "pepsi123", "ParleG"),
+(14.4, 4, "pepsi124", "mazza"),
+(16.4, 4, "pepsi125", "Pepsi"),
+(16.4, 4, "pepsi126", "CocaCola");
